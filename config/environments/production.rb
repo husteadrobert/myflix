9b5e99c49afd1ea53420faa556bdd1803b752537
Robert Hustead <husteadrobert@gmail.com>
2017-12-28 15:12:24 +0900

    production with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,17 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'husteadrobert@gmail.com',
+    password:             'asdfjkl',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+   config.action_mailer.default_url_options = { host: 'https://ls-rh-myflix.herokuapp.com/' }
 end
